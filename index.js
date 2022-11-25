8bc02107da65d758eab6ff5ce314682eb10ba643
Antonio Ortiz <antonioortizdev@gmail.com>
2022-11-25 09:50:48 +0100

    refactor code and wait one second before saying pole

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const tmi = require('tmi.js')
 require('dotenv').config()
 
+const ONE_SECOND = 1000
+
 const appEnv = process.env.APP_ENV
 if (!appEnv) throw new Error('❌ Application environment is not defined.')
 const oauthPassword = process.env.OAUTH_PASSWORD
@@ -13,22 +15,24 @@ const client = new tmi.Client({
 		password: oauthPassword
 	},
 	channels: [ 'olivernabani' ]
-});
+})
+
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs))
+
+const sayPole = async () => {
+  await sleep(ONE_SECOND)
+  client.say(channel, `!pole`)
+  console.log('Oli threw pole!')
+  process.exit()
+}
 
 client.connect();
 
 client.on('message', (channel, tags, message, self) => {
   const author = tags['display-name'].trim().toLowerCase()
-  const messageContent = message.trim().toLowerCase()
-	const authorIsOli = author === 'olivernabani'
-  const authorIsMe = author === 'totti619'
-  const hasOliThrownPole = authorIsOli && messageContent === '!pole'
-  console.log({authorIsOli, authorIsMe, hasOliThrownPole})
+  const messageContent = message.trim()
+  const hasOliThrownPole = author === 'olivernabani' && messageContent === '!pole'
 
-  if (hasOliThrownPole) {
-    client.say(channel, `!pole`);
-    console.log('Oli threw pole!')
-    process.exit()
-  }
+  if (hasOliThrownPole) sayPole()
 });
 		
